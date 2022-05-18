56ac0888731f31b60db265a7f88a9abf244d4e29
전지혜 <jeonjh9535@naver.com>
2022-05-18 17:13:50 +0900

    가게 데이터 표현용

---
+++ app/src/main/java/com/asianaidt/orderapp/datas/StoreDate.kt
@@ -0,0 +1,8 @@
+package com.asianaidt.orderapp.datas
+
+class StoreDate( val name : String,
+                 val logoURL : String,
+                 val score : Double,
+                 val phoneNum : String) {
+
+}
