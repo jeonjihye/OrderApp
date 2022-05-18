e49bcb34f328cd8d1d05b78b57a956182ce4090a
전지혜 <jeonjh9535@naver.com>
2022-05-18 16:59:39 +0900

    탭레이아웃/뷰페이저/프레그먼트 연결완료

--- app/src/main/java/com/asianaidt/orderapp/MainActivity.kt
+++ app/src/main/java/com/asianaidt/orderapp/MainActivity.kt
@@ -2,10 +2,32 @@ package com.asianaidt.orderapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asianaidt.orderapp.Adatpers.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mvpa : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+
+    }
+
+    fun setupEvents(){
+
+    }
+
+    fun setValues(){
+
+        mvpa = MainViewPagerAdapter(supportFragmentManager)
+        mainViewPager.adapter = mvpa
+
+        mainTabLayout.setupWithViewPager(mainViewPager)
+
     }
 }
